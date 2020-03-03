6f214be791118a8f2801ed7e79d70b32542ab5f3
surya04d9 <s.prakash@suntechnoservices.com>
2020-03-03 13:43:59 +0530

    jenkins first

---
+++ src/main/java/com/example/ideapad/IdeapadApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideapad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeapadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeapadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ideapad/IdeapadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideapad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeapadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
